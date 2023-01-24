c4444792e448d6a6ee6d5e0264bf225b9fadf611
Christianscorna <christianscornaienchi@gmail.com>
2023-01-24 12:15:22 -0300

    Todos las partes del cuerpo de la app-web fueron pasados a componentes con AngularCli

---
+++ myPortfolio/main.js
@@ -0,0 +1,4 @@
+window.addEventListener( "load", () => {
+	const currentDate = new Date();
+	yearDate.innerText = currentDate.getFullYear();
+} ) 
--- myPortfolio/src/app/app.module.ts
+++ myPortfolio/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { StudiesComponent } from './components/studies/studies.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ContactMeComponent } from './components/contact-me/contact-me.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarMenuComponent } from './components/nav-bar-menu/nav-bar-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    StudiesComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    FooterComponent,
+    NavBarMenuComponent
   ],
   imports: [
     BrowserModule,
